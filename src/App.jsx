828b33f26f0e58d218d657dc12b2c880c4fa90a9
Navee11 <naveenkumar.r1099@gmail.com>
2024-03-06 21:11:19 +0530

    Input handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [person, setPerson] = useState("");
+  const [people, setPeople] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPeople([...people, person]);
+    setPerson("");
+  };
+
   return (
     <div className="container">
       <h1>Grocery Bud</h1>
       <div>
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
           <div className="form">
-            <input type="text" id="name" placeholder="Enter the groceries" />
+            <input
+              type="text"
+              value={person}
+              id="name"
+              placeholder="Enter the groceries..."
+              onChange={(e) => setPerson(e.target.value)}
+              autoComplete="false"
+            />
             <button type="submit">Submit</button>
           </div>
         </form>
+      </div>
+      <div className="list">
+        {people.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
       </div>
     </div>
   );
