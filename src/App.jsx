db19175c5bfc086665925b5c7ca64ddff7758e50
Navee11 <naveenkumar.r1099@gmail.com>
2024-03-06 20:47:30 +0530

    CSS is completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,6 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  let sampleObject = {
-    key: "example",
-    subProperty1: "value1",
-    subProperty2: "value2",
-    subProperty3: "value3",
-  };
-
-  const {
-    key: id,
-    subProperty1: name,
-    subProperty2: value,
-    subProperty3: age,
-  } = sampleObject;
-  console.log(id, name, age, value);
-
   return (
     <div className="container">
       <h1>Grocery Bud</h1>
